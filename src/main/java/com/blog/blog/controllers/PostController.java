3716822939a79d68fe4febbd2ad1b110fa0d06f7
joshua154 <falkjoshua2200@gmail.com>
2025-07-11 11:32:50 +0200

    some changes

--- src/main/java/com/blog/blog/controllers/PostController.java
+++ src/main/java/com/blog/blog/controllers/PostController.java
@@ -5,6 +5,8 @@ import com.blog.blog.dto.PostCreated;
 import com.blog.blog.entities.Post;
 import com.blog.blog.entities.User;
 import com.blog.blog.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Post", description = "Operations related to posts")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/posts")
@@ -22,10 +25,11 @@ public class PostController {
 
     final private PostService postService;
 
+    @Operation(summary = "Add a post", description = "Add a post to the currently logged in user")
     @PostMapping
     public ResponseEntity<PostCreated> addPost(@AuthenticationPrincipal User user, @Valid @RequestBody  PostCreate postCreate) {
         Post post = postService.addPost(user, postCreate);
-        PostCreated postCreated = new PostCreated(post.getId(), post.getCreatedAt());
+        PostCreated postCreated = new PostCreated(post.getId(), post.getCreatedAt(), post.getAuthor().getId());
 
         return new ResponseEntity<>(postCreated, HttpStatus.CREATED);
     }
--- src/main/java/com/blog/blog/dto/PostCreated.java
+++ src/main/java/com/blog/blog/dto/PostCreated.java
@@ -5,5 +5,5 @@ import org.springframework.lang.NonNull;
 import java.util.Date;
 import java.util.UUID;
 
-public record PostCreated(@NonNull UUID id, @NonNull Date createdAt) {
+public record PostCreated(@NonNull UUID id, @NonNull Date createdAt, UUID authorId) {
 }
