59f37626d2ada9197093e33a29e32c05fee384e1
joshua154 <falkjoshua2200@gmail.com>
2025-07-31 10:18:59 +0200

    customize jwt decoder for demo purposes

--- src/main/java/com/blog/blog/security/SecurityConfig.java
+++ src/main/java/com/blog/blog/security/SecurityConfig.java
@@ -6,13 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.time.Duration;
+import java.util.List;
 
 @Profile("!test")
 @Configuration
@@ -48,8 +50,17 @@ public class SecurityConfig {
         NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder)
                 JwtDecoders.fromIssuerLocation("http://keycloak:8080/realms/blog");
 
-        OAuth2TokenValidator<Jwt> withIssuer = token -> OAuth2TokenValidatorResult.success();
-        jwtDecoder.setJwtValidator(withIssuer);
+        List<String> validIssuers = List.of("http://keycloak:8080/realms/blog", "http://localhost:8080/realms/blog");
+
+        OAuth2TokenValidator<Jwt> customValidator = new DelegatingOAuth2TokenValidator<>(
+                new JwtTimestampValidator(Duration.ofSeconds(60)),
+                token -> {
+                    String iss = token.getIssuer().toString();
+                    return validIssuers.contains(token.getIssuer().toString()) ? OAuth2TokenValidatorResult.success() :
+                            OAuth2TokenValidatorResult.failure(new OAuth2Error("invalid issuer-ur"));
+                });
+
+        jwtDecoder.setJwtValidator(customValidator);
 
         return jwtDecoder;
     }
